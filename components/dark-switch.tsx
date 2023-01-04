945f79cd37d69369d8f6a0273213c7f4297f1f63
CloverMidori <youhuid05@gmail.com>
2023-01-04 19:16:28 +0900

    Fix theme problem

--- components/dark-switch.tsx
+++ components/dark-switch.tsx
@@ -5,10 +5,10 @@ import { useTheme } from 'next-themes'
 
 import { RiMoonFill, RiSunFill } from 'react-icons/ri'
 
-export default function DarkModeSwitch() {
-  const { systemTheme, theme, setTheme } = useTheme()
+import style from "../styles/dark-switch.module.scss"
 
-  const [isOn, setIsOn] = useState(false)
+export default function DarkModeSwitch() {
+  const { resolvedTheme, theme, setTheme } = useTheme()
 
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
@@ -22,15 +22,14 @@ export default function DarkModeSwitch() {
   return (
     <>
       {mounted && (
-        <div onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className={`flex-start flex h-[50px] w-[100px] rounded-[50px] bg-zinc-200 p-[5px] shadow-inner hover:cursor-pointer dark:bg-zinc-700 ${theme === 'dark' ? 'place-content-end' : ""}`}>
+        <div onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')} className={`flex-start flex ${style.TapHighlightColor} h-[50px] w-[100px] rounded-[50px] bg-zinc-200 p-[5px] shadow-inner hover:cursor-pointer dark:bg-zinc-700 ${resolvedTheme === 'dark' ? 'place-content-end' : ""}`}>
           <motion.div
             className="flex h-[40px] w-[40px] items-center justify-center rounded-full bg-white dark:bg-zinc-900"
             layout
             transition={spring}
           >
-
             <motion.div>
-              {!(theme === 'dark') ? (<RiSunFill className="h-6 w-6 text-slate-900" />) : (<RiMoonFill className="h-6 w-6 text-slate-200" />)}
+              {!(resolvedTheme === 'dark') ? (<RiSunFill className="h-6 w-6 text-slate-900" />) : (<RiMoonFill className="h-6 w-6 text-slate-200" />)}
             </motion.div>
           </motion.div>
         </div>
