4a8ed55930a82721eab1a8ebd9f8f00d320ee89a
Clover_Midori <64039436+clover0916@users.noreply.github.com>
2023-01-04 14:53:06 +0900

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
-  'https://pbs.twimg.com/profile_banners/1254230904152535041/1666418834/1500x500'
+  'https://blog.clover-midori.net/assets/blog/authors/clover.png'
