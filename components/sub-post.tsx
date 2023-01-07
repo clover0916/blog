7fab79169242d8bffbd82e49086c83a4d7c3861f
CloverMidori <youhuid05@gmail.com>
2023-01-07 18:12:19 +0900

    Support Responsive

---
+++ components/sub-post.tsx
@@ -0,0 +1,18 @@
+import PostPreview from './post-preview'
+import type Post from '../interfaces/post'
+
+type Props = {
+  posts: Post[]
+}
+
+const SubPost = ({ posts }: Props) => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+        
+      </div>
+    </section>
+  )
+}
+
+export default SubPost
