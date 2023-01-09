f0917413736bd69c4a29968af41128ecee937281
CloverMidori <youhuid05@gmail.com>
2023-01-09 23:26:08 +0900

    Fix displaying title

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter'
-export const CMS_NAME = 'Markdown'
+export const CMS_NAME = 'Clo.Mid'
 export const HOME_OG_IMAGE_URL =
   'https://blog.clover-midori.net/assets/blog/authors/clover.png'
